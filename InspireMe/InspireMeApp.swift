06098e232ddd2121819db62f41f3fcd5fd618e31
Artur Akulich <akulich.artur@gmail.com>
2024-08-08 18:27:28 +0300

    Conflicts resolved

--- InspireMe/InspireMeApp.swift
+++ InspireMe/InspireMeApp.swift
@@ -10,24 +10,8 @@ import SwiftData
 
 @main
 struct InspireMeApp: App {
-<<<<<<< HEAD
-=======
     
     private var homeViewModel = HomeViewModel()
-    
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
->>>>>>> 82972ee6d899c92a474fd8c9fb7db7c1f30616d7
 
     var body: some Scene {
         WindowGroup {
